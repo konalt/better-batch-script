be42902ab92553bcf12dcf6a3c82baba4df501be
konalt <79701511+konalt@users.noreply.github.com>
2021-10-13 16:24:50 +0100

    IT COMPILES

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ const fs = require("fs");
 function compile(filePath) {
     var file = fs.readFileSync(filePath);
     if (!file) throw new Error("Invalid file path specified!");
-    var lines = file.split("\n");
-    logToFile(`Loaded file ${filePath} with ${lines} lines.`);
+    var lines = file.toString().split("\r\n");
+    logToFile(`Loaded file ${filePath} with ${lines.length} lines.`);
     var outFile = "";
 
     lines.forEach(_line => {
@@ -18,9 +18,9 @@ function compile(filePath) {
             args: l.split(" ")
         };
         lineData.args.splice(0, 1);
+        console.log(lineData.args);
         // Parse
         if (l.startsWith("@")) {
-            l = l.substr(1);
             lineData.noShowOutput = true;
         }
         if (l.startsWith("echo")) {
@@ -28,9 +28,27 @@ function compile(filePath) {
                 l = "echo." + lineData.args[0];
             }
         }
+        if (l.startsWith("s0")) {
+            l = "@echo off";
+        }
+        if (l.startsWith("s1")) {
+            l = "@echo on";
+        }
+        outFile += l + "\n";
     });
+    console.log("Writing file...");
+    var outPath = filePath;
+    outPath = outPath.split(".");
+    outPath.pop();
+    outPath.push("bat");
+    fs.writeFileSync(outPath.join("."), outFile);
 }
 
 function logToFile(text) {
-    fs.appendFile("betterbatch.log", text.toString());
+    console.log(text);
+    fs.appendFileSync("betterbatch.log", text.toString() + "\n");
 }
+
+var theRealArgs = process.argv;
+theRealArgs.splice(0, 2);
+compile(theRealArgs.join(" "));
